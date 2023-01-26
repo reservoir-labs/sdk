430797ce21007ccc71cd4da717242d8998163a8b
A.L <xenide@users.noreply.github.com>
2023-01-26 19:13:56 +0800

    fix: bug in getInputAmount

--- src/fetcher.test.ts
+++ src/fetcher.test.ts
@@ -28,7 +28,6 @@ describe('fetcher', () => {
     wallet = new Wallet('0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80', provider)
     const cf = new ContractFactory(GenericFactory.abi, GenericFactory.bytecode, wallet)
     factory = await cf.deploy(wallet.address)
-
     console.log('factory address', factory.address)
   })
 
