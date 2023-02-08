cf29b68bbddacb42d182f69f22af0f18be28c513
Lint Action <lint-action@samuelmeuli.com>
2023-02-08 12:54:18 +0000

    Fix code style issues with Prettier

--- src/router.test.ts
+++ src/router.test.ts
@@ -89,7 +89,7 @@ describe('Router', () => {
           '0x64',
           '0x80',
           [WETH9[1].address, token0.address, token1.address],
-          [0,0],
+          [0, 0],
           '0x0000000000000000000000000000000000000004'
         ])
         expect(result.value).toEqual('0x0')
