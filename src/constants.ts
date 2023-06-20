c3f196a0a0a33fd0e9303c390990c4723dc37abf
A.L <xenide@users.noreply.github.com>
2023-06-20 12:14:19 +0200

    fix: new factory and router addresses

--- src/constants.ts
+++ src/constants.ts
@@ -3,13 +3,13 @@ import { SupportedChainId } from '@reservoir-labs/sdk-core'
 
 export const FACTORY_ADDRESS = {
   // this is deployed using the ReservoirDeployer
-  [SupportedChainId.AVAX]: '0x47e537e1452dbc9c3ee8f1420e5aaf22111d3547',
+  [SupportedChainId.AVAX]: '0xDd723D9273642D82c5761a4467fD5265d94a22da',
   // this is deployed manually as it wasn't necessary to use the ReservoirDeployer at that time
   [SupportedChainId.AVAX_TESTNET]: '0xCae997a6f253814441B878868fd6DBB32a52816f'
 }
 
 export const ROUTER_ADDRESS = {
-  [SupportedChainId.AVAX]: '0x67bc78378723acd8876FCA16d9c6C24ff79acb8e',
+  [SupportedChainId.AVAX]: '0x9Fc6B082DfB632bC11156f6fD2dc5F97F9B865F0',
   [SupportedChainId.AVAX_TESTNET]: '0xd627FdC984a249E9b5F2df263A37368f4e459726'
 }
 export const MINIMUM_LIQUIDITY = JSBI.BigInt(1000)
