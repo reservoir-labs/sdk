aa85f6fa34ff65c55bea1018143528a1f20220e6
A.L. <xenide@users.noreply.github.com>
2024-12-24 19:59:05 +0800

    fix: prepare for new addresses

--- src/constants.ts
+++ src/constants.ts
@@ -3,13 +3,13 @@ import { SupportedChainId } from '@reservoir-labs/sdk-core'
 
 export const FACTORY_ADDRESS = {
   // this is deployed using the ReservoirDeployer
-  [SupportedChainId.AVAX]: '0xDd723D9273642D82c5761a4467fD5265d94a22da',
-  [SupportedChainId.AVAX_TESTNET]: '0xDd723D9273642D82c5761a4467fD5265d94a22da'
+  [SupportedChainId.ARBITRUM]: '',
+  [SupportedChainId.ARBITRUM_SEPOLIA]: ''
 }
 
 export const ROUTER_ADDRESS = {
-  [SupportedChainId.AVAX]: '0x9Fc6B082DfB632bC11156f6fD2dc5F97F9B865F0',
-  [SupportedChainId.AVAX_TESTNET]: '0xC7e9e9c1C9Aef23cAA4c885D7FEA876696DaC538'
+  [SupportedChainId.ARBITRUM]: '',
+  [SupportedChainId.ARBITRUM_SEPOLIA]: ''
 }
 export const MINIMUM_LIQUIDITY = JSBI.BigInt(1000)
 export const FEE_ACCURACY = JSBI.BigInt(1_000_000) // 100%
