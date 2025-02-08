5da4fdf67108cf0159454caaff92ae6af20a805b
A.L. <xenide@users.noreply.github.com>
2025-02-08 20:01:10 +0700

    chore: add arbitrum mainnet addresses

--- src/constants.ts
+++ src/constants.ts
@@ -3,12 +3,12 @@ import { SupportedChainId } from '@reservoir-labs/sdk-core'
 
 export const FACTORY_ADDRESS = {
   // this is deployed using the ReservoirDeployer
-  [SupportedChainId.ARBITRUM]: '',
+  [SupportedChainId.ARBITRUM]: '0x89D235b4A770CB09ee976dF814266226A23A9315',
   [SupportedChainId.ARBITRUM_SEPOLIA]: '0x89D235b4A770CB09ee976dF814266226A23A9315',
 }
 
 export const ROUTER_ADDRESS = {
-  [SupportedChainId.ARBITRUM]: '',
+  [SupportedChainId.ARBITRUM]: '0x84baf4228D31595370BDCD02ad2d9467Ba1cADAf',
   [SupportedChainId.ARBITRUM_SEPOLIA]: '0x0E177118dC36B78D9cc7F018d82090208601e467',
 }
 export const MINIMUM_LIQUIDITY = JSBI.BigInt(1000)
