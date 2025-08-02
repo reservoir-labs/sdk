7144b9dc04192c2ce55c275ffb8757c4f1cb2352
A.L. <xenide@users.noreply.github.com>
2025-08-02 11:45:39 +0100

    fix: add jest stuff

---
+++ jest.config.js
@@ -0,0 +1,17 @@
+/** @type {import('jest').Config} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  // Only run tests in the SDK source tree, ignore vendored sub-modules
+  testMatch: ['<rootDir>/src/**/*.test.ts', '<rootDir>/src/**/*.spec.ts'],
+  modulePathIgnorePatterns: [
+    '<rootDir>/lib/',
+    '<rootDir>/dist/',
+    '<rootDir>/node_modules/'
+  ],
+  globals: {
+    'ts-jest': {
+      tsconfig: 'tsconfig.json'
+    }
+  }
+}
